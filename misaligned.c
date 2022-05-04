a9662e35945a8e9b1c9a5759a4b95a17712554bd
malinimalayappan97 <100181333+malinimalayappan97@users.noreply.github.com>
2022-05-04 08:16:25 +0530

    Update misaligned.c
    failing the test

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,60 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* majorColorNames[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+int numberOfMajorColors =
+    sizeof(majorColorNames) / sizeof(majorColorNames[0]);
+
+int numberOfMinorColors =
+    sizeof(minorColorNames) / sizeof(minorColorNames[0]);
+
+typedef struct {
+    enum MajorColor majorColorValue;
+    enum MinorColor minorColorValue;
+} ColorPair;
+
+
+
+int printColorMap() {    
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);
+            printf("%d | %s | %s\n", ((i*5)+j+1), majorColorNames[i], minorColorNames[j]);
         }
     }
     return i * j;
 }
 
+
+ColorPair GetColorFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    colorPair.majorColorValue = 
+        (enum MajorColor)(pairNumber / numberOfMinorColors);
+    colorPair.minorColorValue =
+        (enum MinorColor)(pairNumber % numberOfMinorColors);
+    return colorPair;
+}
+
+void testNumberToPair(int pairNumber,
+    enum MajorColor expectedMajor,
+    enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    assert(colorPair.minorColorValue == expectedMinor);
+    assert(colorPair.majorColorValue == expectedMajor);
+}
+
 int main() {
-    int result = printColorMap();
-    assert(result == 25);
+    printColorMap();
+    testNumberToPair(25,VIOLET,SLATE);
+    testNumberToPair(1,WHITE,BLUE);
+    testNumberToPair(19,YELLOW,BROWN);
+    
     printf("All is well (maybe!)\n");
     return 0;
 }
